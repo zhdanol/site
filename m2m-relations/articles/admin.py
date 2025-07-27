da45df5343d3629c78b0f50fafd5b35789fa99b7
zhdanol <olegzdanov87@gmail.com>
2025-07-27 17:00:09 +0700

    commit

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -7,19 +7,22 @@ from .models import Scope, Article, Topic
 
 class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
+        checked = 0  # Инициализация переменной перед использованием
+        
         for form in self.forms:
-            # В form.cleaned_data будет словарь с данными
-            # каждой отдельной формы, которые вы можете проверить
-            if form.cleaned_data.get('is_main') == True:
+            # Проверяем, есть ли данные формы и не удалена ли форма
+            if form.cleaned_data.get('DELETE', False):
+                continue  # Пропускаем удаленные формы
+                
+            if form.cleaned_data.get('is_main', False):
                 checked += 1
-            # вызовом исключения ValidationError можно указать админке о наличие ошибки
-            # таким образом объект не будет сохранен,
-            # а пользователю выведется соответствующее сообщение об ошибке
+                
         if checked > 1:
             raise ValidationError('Основным может быть только один раздел')
         elif checked == 0:
-            raise ValidationError('Тут всегда ошибка')
-        return super().clean()  # вызываем базовый код переопределяемого метода
+            raise ValidationError('Укажите основной раздел')
+            
+        return super().clean()  # Вызываем базовый метод
 
 
 class RelationshipInline(admin.TabularInline):
@@ -31,6 +34,7 @@ class RelationshipInline(admin.TabularInline):
 class ObjectAdmin(admin.ModelAdmin):
     inlines = [RelationshipInline]
     
+
 @admin.register(Topic)
 class ScopesAdmin(admin.ModelAdmin):
     pass
--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -1,16 +1,18 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from pprint import pprint
 
+from django.shortcuts import render
 from articles.models import Article
 
 
 def articles_list(request):
     template = 'articles/news.html'
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
-    objects_list = Article.objects.all().order_by(ordering).prefetch_related('scopes')
-    context = {'objects_list': objects_list}
+    object_list = Article.objects.all().order_by(ordering).prefetch_related('scopes')
+
+
+
+
+    context = {'object_list': object_list}
+
 
     return render(request, template, context)
